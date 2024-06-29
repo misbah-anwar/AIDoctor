50e6a4aa511d07f53c15c9b129010afdb2732263
Misbah Anwar <misbahanwar1802@gmail.com>
2024-06-29 21:54:52 +0530

    Update aidoctor.py

--- aidoctor.py
+++ aidoctor.py
@@ -1,23 +1,13 @@
 import streamlit as st
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
-import torch
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
-# Load model and tokenizer
 @st.cache_resource
 def load_model():
-    model = AutoModelForCausalLM.from_pretrained(
-        "aboonaji/llama2finetune-v2",
-        quantization_config=BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=getattr(torch, "float16"),
-            bnb_4bit_quant_type="nf4"
-        )
-    )
+    model = AutoModelForCausalLM.from_pretrained("aboonaji/llama2finetune-v2")
     tokenizer = AutoTokenizer.from_pretrained("aboonaji/llama2finetune-v2", trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
     return model, tokenizer
 
-# Main Streamlit app
 def main():
     st.title("AI Doctor")
 
